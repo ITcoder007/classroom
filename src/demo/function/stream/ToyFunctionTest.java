a2418116b9b60a999440af2a906d5b05b5782ec1
star <itcoder007@126.com>
2023-07-23 19:21:58 +0800

    函数式编程 demo fix

--- src/demo/function/stream/ToyFunctionTest.java
+++ src/demo/function/stream/ToyFunctionTest.java
@@ -13,8 +13,8 @@ public class ToyFunctionTest {
         // 直接调用
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter isoDateTime = DateTimeFormatter.ISO_DATE_TIME;
-        String format = ToyFunctionTest.dateFormat(now, isoDateTime);
-        System.out.println( format );
+        String res = ToyFunctionTest.dateFormat(now, isoDateTime);
+        System.out.println( res );
 
         // 匿名内部类
         ToyFunction<String, LocalDateTime, DateTimeFormatter> toyFunction = new ToyFunction<String, LocalDateTime, DateTimeFormatter>() {
@@ -24,18 +24,16 @@ public class ToyFunctionTest {
                 return ToyFunctionTest.dateFormat(dateTime, dateTimeFormatter);
             }
         };
-        String res = toyFunction.run(now, isoDateTime);
+        res = toyFunction.run(now, isoDateTime);
         System.out.println( res );
 
         // 匿名内部类 --简化--> Lambda 表达式
-        toyFunction = (LocalDateTime dateTime, DateTimeFormatter dateTimeFormatter) -> {
-                return ToyFunctionTest.dateFormat(dateTime, dateTimeFormatter);
-        };
+        toyFunction = (LocalDateTime dateTime, DateTimeFormatter dateTimeFormatter) -> ToyFunctionTest.dateFormat(dateTime, dateTimeFormatter);
         res = toyFunction.run(now, isoDateTime);
         System.out.println( res );
 
         // 函数方法引用
-        ToyFunction<String, LocalDateTime, DateTimeFormatter> toyFunction2 = ToyFunctionTest::dateFormat;
+        toyFunction = ToyFunctionTest::dateFormat;
         res = toyFunction.run(now, isoDateTime);
         System.out.println( res );
 
